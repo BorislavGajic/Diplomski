ede0f491b2592dc1981f22f20875024febd489ab
BorislavGajic <borawr22@gmail.com>
2021-03-25 14:41:28 +0100

    Create data.sql
    Dod1

---
+++ Diplomski/target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO kancelarija (kol_ormanak, kol_racunara, kol_stolicak, kol_stolovak)
+VALUES ('50', '50', '50', '50');
+
+INSERT INTO magacin (kol_ormanam, kol_paleta, kol_viljuskara, max_kol_materijala)
+VALUES ('50', '50', '50', '50000');
+
+INSERT INTO pogon (kol_masinap, kol_ormanap, kol_stolicap, kol_stolovap)
+VALUES ('50', '50', '50', '50');
